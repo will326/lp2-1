62694cbbd710615e3795781f1a3f0b43b8f9b2d5
Slavex008 <arthucruz@hotmail.com>
2015-03-17 10:55:30 -0300

    Create Exercicio1_3.java
    Exercicio 3 do dia 17/03

---
+++ exercicio1/Exercicio1_3.java
@@ -0,0 +1,16 @@
+public class Exercicio1_3 {
+	public static void main(String [] args){
+		int a, b, c, soma, mult, sub, divi;		
+		a = 45;
+		b = 20;
+		c = 3;		
+		soma = a + b + c;
+		mult = a * b * c;
+		divi = a / b / c;
+		sub = a - b - c;
+		System.out.println("O valor da soma foi: " + soma);
+		System.out.println("O valor da multiplicação foi: " + mult);
+		System.out.println("O valor da divisão foi: " + divi);
+		System.out.println("O valor da subtração foi: " + sub);
+	}
+}
