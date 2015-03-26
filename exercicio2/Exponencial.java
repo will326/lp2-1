9b8f94692ebe7b9186b2a91e31458bbc1fe9027b
Slavex008 <arthucruz@hotmail.com>
2015-03-26 14:07:14 -0300

    Create Exponencial.java
    exercicio 2 numero 6 sobre POO

---
+++ exercicio2/Exponencial.java
@@ -0,0 +1,11 @@
+public class Exponencial{	
+	public static void main(String [] args){
+		int var=0;		
+		System.out.println("# Q C");
+        while (var <= 10 ){
+			System.out.println(var + " " + var * var + " " + var * var * var);
+			var++;		
+		}
+
+	}	
+}
